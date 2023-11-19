3efc466ed5bf435b3b86f86bd6586cd554cf6fd0
daisuke.abe <commityneo@gmail.com>
2023-11-19 19:22:30 +0900

    Initial Commit

---
+++ greetingBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  greetingBox
+//
+//  Created by 阿部大輔 on 2023/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ greetingBox/greetingBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  greetingBoxApp.swift
+//  greetingBox
+//
+//  Created by 阿部大輔 on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct greetingBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
